2c4ec28a76144d1ddfce2f815a6f1e2861bbe091
Ramazan <200107095@stu.sdu.edu.kz>
2022-10-25 19:51:55 +0600

    Adding pictures for menu

--- app/src/main/java/com/example/cumpot/MainActivity.kt
+++ app/src/main/java/com/example/cumpot/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.cumpot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("Imaka hui sosi")
+        val photo1: ImageView = findViewById(R.id.imageView)
+        photo1.setImageResource(R.drawable.photo_2022_10_25_19_28_45)
     }
 }
