9edbe7d8aff8709d080b228c012a75a13951beba
Ramazan <200107095@stu.sdu.edu.kz>
2022-11-03 22:19:45 +0600

    Added navigation and fragments

--- app/src/main/java/com/example/cumpot/MainActivity.kt
+++ app/src/main/java/com/example/cumpot/MainActivity.kt
@@ -2,13 +2,10 @@ package com.example.cumpot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val photo1: ImageView = findViewById(R.id.imageView)
-        photo1.setImageResource(R.drawable.photo_2022_10_25_19_28_45)
     }
 }
